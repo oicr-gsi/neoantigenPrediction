8a1842356e409dec089abccb361bd7ba120182fc
Lawrence Heisler <lawrence.heisler@oicr.on.ca>
2025-03-24 13:41:42 -0400

    modified calculate.sh to exclude xlsx and to exclude columns from tsv

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,9 +5,11 @@ set -o pipefail
 
 cd $1
 
-md5sum *.tsv
+### exclude columns from tsv with precision values
+cat *.tsv | cut -f 1-10,13-26,28,30-36 | md5sum
 
-xlsxfile=`ls *.xlsx | head -n 1`
-unzip -q $xlsxfile -d xlsx
-find xlsx -name *.xml | xargs md5sum | grep -v core.xml
+### below will check the xlsx, but will not run due to precision values existing in worksheets which is hard to manage
+#xlsxfile=`ls *.xlsx | head -n 1`
+#unzip -q $xlsxfile -d xlsx
+#find xlsx -name *.xml | xargs md5sum | grep -v core.xml
 
